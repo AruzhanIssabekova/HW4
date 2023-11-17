5d5a0df419a199adb41f8da2a05dfe9d216ba482
AruzhanIssabekova <aruzhan.issabekova123@gmail.com>
2023-11-18 01:25:02 +0600

    HW4

---
+++ HW4/HW4.py
@@ -0,0 +1,65 @@
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a+b>c or a+c>b or b+c>a:
+#     print(True)
+# else:
+#     print(False)
+
+# a=int(input())
+# if a%2==0:
+#     print(True)
+# else:
+#     print(False)
+
+# a=int(input())
+# if a+b>c:
+#     print(True)
+# else:
+#     print(False)
+
+# a=int(input())
+# b=int(input())
+# if a>b:
+#     print(True)
+# else:
+#     print(False)
+
+# l=input('login:')
+# p=input('password:')
+# if l == 'user' and p == 'qwerty':
+#     print('Authentication completed')
+# else:
+#     print('Invalid login or password')
+
+# tg=int(input('Insert amount in KZT:'))
+# a=int(input('Choose currency: '
+#         '[1] USD  '
+#         '[2] EUR  '
+#         '[3] RUB '))
+# if a == 1:
+#     print(rond (tg/420, 2), 'USD')
+# elif a == 2:
+#     print(round(tg/510, 2), 'EUR')
+# elif a == 3:
+#     print(round(tg/5.8, 2), 'RUB')
+
+# n=int(input())
+# a6=n%10
+# a5=(n//10)%10
+# a4=(n//100)%10
+# a3=(n//1000)%10
+# a2=(n//10000)%10
+# a1=(n//100000)%10
+# if (a1+a2+a3) == (a4+a5+a6):
+#     print('Счастливый')
+# else:
+#     print('Обычный')
+
+# s=int(input())
+# p=int(input())
+# m=int(input())
+# if m>=s+p:
+#     print('Da')
+# else:
+#     print('Net')
